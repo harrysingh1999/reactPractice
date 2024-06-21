f1cfced1c6043a9e9f8318b745a73100ba5949ba
Harvinder-paytel <harvinderpaytel@gmail.com>
2024-06-21 16:58:57 +0530

    added ScrollTo functionality in routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom";
-import React, { Suspense } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
 import Home from "./Components/Home";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <>
       <Home />
