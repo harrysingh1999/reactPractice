afd36beb42401cb9e9ec649a10a2d8e9a34d4376
Harvinder Singh <hs541156@gmail.com>
2024-09-12 19:56:04 +0530

    final commit

--- src/Components/StopWatch.jsx
+++ src/Components/StopWatch.jsx
@@ -3,25 +3,33 @@ import React, { useEffect, useState } from "react";
 export default function StopWatch() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
+  const [text2, setText2] = useState("");
 
   useEffect(() => {
     const timer = setInterval(() => {
-      if (text === "restart") {
+      if (text2 === "restart") {
         setCount(0);
-        setText('')
+        setText("");
+        setText2("");
       }
-      if (text === "pause") {
+      if (text2 === "pause") {
         clearInterval(timer);
-      } else {
+      }
+      if (text2 === "" || text2 === "start") {
         setCount((prevCount) => prevCount + 1);
       }
-    }, 1000);
+    }, 800);
 
     return () => clearInterval(timer);
-  }, [text]);
+  }, [text, text2]);
 
   const handleChange = (e) => {
     setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    text === "" ? setText2("hold") : setText2(text);
   };
 
   return (
@@ -29,15 +37,19 @@ export default function StopWatch() {
       <h1 className="text-4xl text-center font-semibold">StopWatch</h1>
       <p>Count is {count}</p>
 
-      <input
-        type="text"
-        placeholder="enter"
-        className="border border-black mb-4"
-        value={text}
-        onChange={(e) => handleChange(e)}
-      />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="enter"
+          className="border border-black mb-4"
+          value={text}
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="border border-black p-1">Submit</button>
+      </form>
 
       <p>Type 'pause' to pause the count</p>
+      <p>Type 'start' to start the count, from where you paused it</p>
       <p>Type 'restart' to restart the count</p>
     </div>
   );
